3878dc9396cb33e6c7328456b15dad706694dc77
Sunnyinho <sunnnny619@gmail.com>
2022-11-14 17:08:52 +0545

    added versions as git

--- 3_pydantin_model.py
+++ 3_pydantin_model.py
@@ -1,3 +1,4 @@
+import os
 from prefect import flow, task
 from pydantic import BaseModel
 
@@ -10,8 +11,9 @@ class Model(BaseModel):
 def printer(obj):
     print(f"Received a {type(obj)} with value {obj}")
 
-@flow
+@flow(version=os.getenv("GIT_COMMIT_SHA"))
 def model_validator(model: Model):
     printer(model)
 
 model_validator({"a": 42, "b": 0, "c": 55})
+print(os.getenv("GIT_COMMIT_SHA"))
